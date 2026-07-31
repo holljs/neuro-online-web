956d4713e3e9037dae2decf4b726cf08ca8e4e90
holljs <117600610+holljs@users.noreply.github.com>
2026-07-31 13:28:10 +0300

    репетитор

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import SettingsPage from "./pages/SettingsPage";
 import KidsHub from "./pages/KidsHub";
 import MalyshPage from "./pages/KidsHub/Malysh";
 import GeniyPage from "./pages/KidsHub/Geniy";
+import RepetitorPage from "./pages/KidsHub/Repetitor";
 
 export default function App() {
   return (
@@ -40,6 +41,7 @@ export default function App() {
             <Route path="/kids" element={<KidsHub />} />
             <Route path="/kids/malysh" element={<MalyshPage />} />
             <Route path="/kids/geniy" element={<GeniyPage />} />
+            <Route path="/kids/repetitor" element={<RepetitorPage />} />
             <Route path="/history" element={<HistoryPage />} />
 
             {/* Others Page */}
