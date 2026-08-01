e5f5f48d3b28dc14ccd8c4294cb6d6997368a1d9
holljs <117600610+holljs@users.noreply.github.com>
2026-08-01 22:22:41 +0300

    шрифт

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -128,10 +128,10 @@ export default function UserDropdown() {
               </Link>
             </li>
 
-            {/* 2. Настройки */}
+            {/* 2. Настройки (🔥 Перенаправление ведет на /settings) */}
             <li>
               <Link
-                to="/profile"
+                to="/settings"
                 onClick={closeDropdown}
                 className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300 cursor-pointer"
               >
